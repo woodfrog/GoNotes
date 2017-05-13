fddf17fe47da7580098ec0d43ab4ccd3e8272539
CJC <cjc0722hz@gmail.com>
2017-05-13 14:54:40 -0700

    fmt all

--- fib_closure.go
+++ fib_closure.go
@@ -5,24 +5,23 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
-        prev := 0
-            next := 1
-            return func() int {
-                        tmp := prev
-                                prev = next
-                                        next = tmp + next
-                                                return tmp
-                                                    
-            }
+	prev := 0
+	next := 1
+	return func() int {
+		tmp := prev
+		prev = next
+		next = tmp + next
+		return tmp
+
+	}
 
 }
 
 func main() {
-        f := fibonacci()
-        for i := 0; i < 10; i++ {
-                    fmt.Println(f())
-                        
-        }
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+
+	}
 
 }
-
--- maps.go
+++ maps.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-        "golang.org/x/tour/wc"
-            "strings"
-
+	"golang.org/x/tour/wc"
+	"strings"
 )
 
-func WordCount(s string)  (result map[string]int) {
-        result = make(map[string]int)
-            words := strings.Fields(s)
-            for _, word := range words {
-                        result[word] += 1
-                            
-            }
-                return 
+func WordCount(s string) (result map[string]int) {
+	result = make(map[string]int)
+	words := strings.Fields(s)
+	for _, word := range words {
+		result[word] += 1
+
+	}
+	return
 
 }
 
 func main() {
-        wc.Test(WordCount)
+	wc.Test(WordCount)
 
 }
-
